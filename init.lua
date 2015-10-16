33aab2348b31502b9c5e93a78105286e893aa738
tommasoR <tommaso.ruggiero@gmail.com>
2015-10-16 15:16:46 +0200

    versione alfa 1

--- init.lua
+++ init.lua
@@ -3,7 +3,9 @@ print("set up wifi mode e configuro in e out")
 gpio.mode(4,gpio.OUTPUT)
 --Utilizzato come input 
 gpio.mode(3,gpio.INPUT,gpio.PULLUP)
-time=0
+timeOn=0
+timeOff=0
+local StrUltimoAllarme='' 
 
 wifi.setmode(wifi.SOFTAP)
 local cfg
@@ -38,23 +40,51 @@ collectgarbage()
 
 
 allarmOn=0
+
 tmr.alarm(0,500,1,function()
-print("\r\n**allarmOn:",allarmOn)
-if gpio.read(3)== gpio.LOW then 
-    allarmOn=1 
-    gpio.write(4,gpio.HIGH)
-else 
-    allarmOn=0 
-    gpio.write(4,gpio.LOW) 
+--print("\r\n**allarmOn:",allarmOn)
+if (gpio.read(3) == gpio.LOW and allarmOn == 0) then 
+    allarmOn=1
+	timeOn=tmr.now()
+    gpio.write(4,gpio.HIGH)   
 end 
+if allarmOn == 1 then
+	disattivaSirena()
+end
 end)
+
+
+function disattivaSirena()
+    --print(tmr.now())
+	--print (gpio.read(4))
+	--300000000 = 5 minuti
+	if(tmr.now()-timeOn> 300000000 and allarmOn == 1) then 
+		allarmOn = 2
+		timeOff=tmr.now()
+		gpio.write(4,gpio.LOW)
+	end
+end
 
 local srv=net.createServer(net.TCP) 
 srv:listen(80,function(conn) 
     conn:on("receive",function(conn,payload) 
     print(payload) 
-    conn:send("<h1> ESP8266<BR>Server is working!</h1><BR>allarmOn=")
+	print("\r\n**allarmOn:",allarmOn)
+	print("\r\n**timeOn:",timeOn)
+	print("\r\n**timeOff:",timeOff)
+	print("\r\n** rele su gpio.read(4):",gpio.read(4))
+	print("\r\n** contatto su gpio.read(4):",gpio.read(3))
+    conn:send("<h1> Allarme giardino<BR>Server is working!<BR>allarmOn=")
 	conn:send(allarmOn)
+	conn:send("<BR>Ultimo Allarme minuti passati=")
+	if (allarmOn >0) then
+		conn:send((tmr.now()-timeOn)/60000000)
+		conn:send("<BR>Ha suonato per Minuti:")
+		conn:send((timeOff-timeOn)/60000000)
+	else 
+		conn:send("Mai suonato")
+	end
+	conn:send("</h1>")
     conn:close()
 	end) 
 end)
